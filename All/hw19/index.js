c6d0f60a22b1d9b82bdcd995e920ac53525d20c7
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-06-12 20:39:50 +0300

    Add files via upload

---
+++ All/hw19/index.js
@@ -0,0 +1,26 @@
+const array = [
+    [
+      ['HELLO WORLD!!!', true, [1,2,3,4, [10, 20]]],
+    ],
+    [
+      [1, 2, 3, [100, 200]],
+      ['name', 'male']
+    ],
+  ];
+  
+  function deepClone(array) {
+
+    const newArray = [];
+    for (i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        newArray.push(array[i].slice())
+      } else {
+        newArray.push(array[i]);
+      }
+    }
+  
+    return newArray;
+  }
+  
+  cloneArray = deepClone(array);
+  console.log(cloneArray);
